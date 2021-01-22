234720bc467bf7aebb57837e25bf0098a72c716f
sakurasou3 <76951074+sakurasou3@users.noreply.github.com>
2021-01-22 22:35:33 +0900

    add slick.js

--- corporate_site/assets/js/common.js
+++ corporate_site/assets/js/common.js
@@ -18,16 +18,40 @@ $(function () {
             // 非表示
             topBtn.fadeOut();
         }
+    });
 
-        // ボタンのクリック動作をaタグの挙動から上書きする
-        topBtn.on("click", function(event) {
-            // console.log(event);
-            event.preventDefault(); // ページ内リンクの挙動をキャンセル
+    // ボタンのクリック動作をaタグの挙動から上書きする
+    topBtn.on("click", function(event) {
+        // console.log(event);
+        event.preventDefault(); // ページ内リンクの挙動をキャンセル
 
-            // スクロールさせるアニメーション設定
-            $("body,html").animate({
-                scrollTop: 0
-            }, 500);
-        });
+        // スクロールさせるアニメーション設定
+        $("body,html").animate({
+            scrollTop: 0
+        }, 500);
+    });
+
+    // ナビゲーションメニュー
+    // ①slideToggleを使用しない場合の表記方法
+    // $(".close-button").on("click", function() {
+    //     $(".header nav").slideUp();
+    // });
+    // $(".open-button").on("click", function() {
+    //     $(".header nav").slideDown();
+    // });
+
+    // ②slideToggleを使用するとシンプルに書ける
+    $(".close-button, .open-button").on("click", function() {
+        $('.header nav').slideToggle();
+    });
+
+    // slick
+    // https://kenwheeler.github.io/slick/
+    $('#slick-slide').slick({
+        autoplay: true,
+        autoplaySpeed: 2000,
+        dots: true,
+        arrows: false,
+        fade: true
     });
 });
